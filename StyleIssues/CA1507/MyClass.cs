4147b737b4866706ece02ee2c5db3669447afa5a
Maks <maksimyaremchuk06@gmail.com>
2024-12-25 13:52:15 +0200

    Initial commit after re-initializing the repository

---
+++ StyleIssues/CA1507/MyClass.cs
@@ -0,0 +1,15 @@
+﻿namespace StyleIssues.CA1507
+{
+    public static class MyClass
+    {
+        public static string MyMethod(string str)
+        {
+            if (string.IsNullOrEmpty(str))
+            {
+                throw new ArgumentNullException(nameof(str));
+            }
+
+            return "test" + str;
+        }
+    }
+}
